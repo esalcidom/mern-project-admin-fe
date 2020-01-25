6ca17ba0f12b21daa65167e4010014c8f03bb3f6
Emmanuel Salcido <salcidoe@amdocs.com>
2020-01-24 18:42:57 -0600

    .

---
+++ src/context/proyectos/ProyectoState.js
@@ -0,0 +1,26 @@
+import React, { useReducer } from 'react';
+import proyectoContext from './ProyectoContext';
+import proyectoReducer from './ProyectoReducer';
+
+const ProyectoState = props => {
+    const initialState = {
+        formulario : false
+    }
+
+    //Dispatch para ejecutar las acciones
+    const [ state, dispatch ] = useReducer(proyectoReducer, initialState);
+
+    //serie de funciones para el CRUD
+
+    return(
+        <proyectoContext.Provider
+            value={{
+                formulario: state.formulario
+            }}
+        >
+            {props.children}
+        </proyectoContext.Provider>
+    )
+}
+
+export default ProyectoState;
